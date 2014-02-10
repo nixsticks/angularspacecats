ddce69734b16f086c1cb4df36a7b179894901cbc
Nikki Thean <nikki.thean@gmail.com>
2014-02-10 14:27:21 -0500

    Get routes to work

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var spacecatApp = angular.module('spacecatApp', ['ngRoute', 'spacecatControllers']);
+
+spacecatApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/cats', {
+    templateUrl: 'partials/cat-list.html',
+    controller: 'CatListCtrl'
+  }).
+  when('/cats/:catId', {
+    templateUrl: 'partials/cat-detail.html',
+    controller: 'CatDetailCtrl'
+  }).
+  otherwise({
+    redirectTo: '/cats'
+  });
+}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,17 @@
 'use strict';
 
-var spacecatApp = angular.module('spacecatApp', []);
+var spacecatControllers = angular.module('spacecatControllers', []);
 
-spacecatApp.controller('CatListCtrl', function($scope, $http){
-  
-  $http.get('cats/cats.json').success(function(data){
+spacecatControllers.controller('CatListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('cats/cats.json').success(function(data) {
     $scope.cats = data;
   });
 
   $scope.orderProp = 'name';
-});
+}]);
+
+spacecatControllers.controller('CatDetailCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('cats/cats-' + $routeParams.catId + '.json').success(function(data) {
+    $scope.cat = data[0];
+  });
+}]);
